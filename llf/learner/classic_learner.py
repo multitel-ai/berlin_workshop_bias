7d7af1722a14dc15c50b125fa518fec7ba5187ae
gerald4 <geraldin.nanfack@unamur.be>
2022-09-12 10:54:59 +0200

    putting lff inside one folder

---
+++ llf/learner/classic_learner.py
@@ -0,0 +1,230 @@
+'''
+This code is inspired form https://github.com/alinlab/LfF/blob/master/train_vanilla.py
+'''
+
+import os
+import pickle
+from tqdm import tqdm
+from datetime import datetime
+
+import numpy as np
+
+import torch
+from torch.utils.data import DataLoader, RandomSampler, BatchSampler, WeightedRandomSampler
+from torch.utils.data.dataset import Subset
+from torchvision import transforms as T
+import torch.nn.functional as F
+import torch.nn as nn
+
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    from torch.utils.tensorboard import SummaryWriter
+
+from data.utils import get_dataset, IdxDataset, ZippedDataset
+from models.models import get_model, GeneralizedCELoss
+from .utils import MultiDimAverageMeter, EMA
+
+
+
+def train(
+    main_tag,
+    dataset_tag,
+    model_tag,
+    data_dir,
+    log_dir,
+    device,
+    target_attr_idx,
+    bias_attr_idx,
+    main_num_steps,
+    main_valid_freq,
+    main_batch_size,
+    main_learning_rate,
+    main_weight_decay,
+    percent
+):
+
+    
+    device = torch.device(device)
+    start_time = datetime.now()
+    
+    writer = SummaryWriter(os.path.join(log_dir, "summary", main_tag))
+
+    print(dataset_tag)
+
+    #Reading training and validation data
+    train_dataset = get_dataset(
+        dataset_tag,
+        data_dir=data_dir,
+        dataset_split="train",
+        transform_split="train",
+        percent=percent
+    )
+    valid_dataset = get_dataset(
+        dataset_tag,
+        data_dir=data_dir,
+        dataset_split="valid",
+        transform_split="valid",
+        percent= percent                             
+    )
+
+    '''Getting the number of classes and 
+
+    domain of biaises (just for evaluation since the method does not assume and existing bias)
+    '''
+
+    train_target_attr = []
+    for data in train_dataset.data:
+        train_target_attr.append(int(data.split('_')[-2]))
+    train_target_attr = torch.LongTensor(train_target_attr)
+
+    attr_dims = []
+    attr_dims.append(torch.max(train_target_attr).item() + 1)
+    num_classes = attr_dims[0] 
+        
+    #IdxDataset just add the first element of idx before x
+    train_dataset = IdxDataset(train_dataset)
+    valid_dataset = IdxDataset(valid_dataset)    
+
+    # make loader    
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=main_batch_size,
+        shuffle=True,
+        num_workers=8,
+        pin_memory=True,
+    )
+
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=256,
+        shuffle=False,
+        num_workers=8,
+        pin_memory=True,
+    )
+    
+    # define model and optimizer
+    model = get_model(model_tag, attr_dims[0]).to(device)
+    optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=main_learning_rate,
+            weight_decay=main_weight_decay,
+        )
+
+    # define loss
+    criterion = torch.nn.CrossEntropyLoss()
+    label_criterion = torch.nn.CrossEntropyLoss(reduction="none")
+
+    # define evaluation function
+    def evaluate(model, data_loader):
+        model.eval()
+        acc = 0
+        total_correct, total_num = 0, 0
+        #attrwise_acc_meter = MultiDimAverageMeter(attr_dims)
+        for index, data, attr, _ in tqdm(data_loader, leave=False):
+            label = attr[:, target_attr_idx]
+            data = data.to(device)
+            attr = attr.to(device)
+            label = label.to(device)
+            with torch.no_grad():
+                logit = model(data)
+                pred = logit.data.max(1, keepdim=True)[1].squeeze(1)
+                correct = (pred == label).long()
+                total_correct += correct.sum()
+                total_num += correct.shape[0]
+
+
+        accs = total_correct/float(total_num)
+
+        model.train()
+
+        return accs
+
+    # define extracting indices function
+    def get_align_skew_indices (lookup_list, indices):
+        '''
+        lookup_list:
+            A list of non-negative integer. 0 should indicate bias-align sample and otherwise(>0) indicate bias-skewed sample.
+            Length of (lookup_list) should be the number of unique samples
+        indices:
+            True indices of sample to look up.
+        '''
+        pseudo_bias_label = lookup_list[indices]
+        skewed_indices = (pseudo_bias_label != 0).nonzero().squeeze(1)
+        aligned_indices = (pseudo_bias_label == 0).nonzero().squeeze(1)
+
+        return aligned_indices, skewed_indices
+
+
+    valid_attrwise_accs_list = []
+
+    for step in tqdm(range(main_num_steps)):
+        try:
+            index, data, attr, _ = next(train_iter)
+        except:
+            train_iter = iter(train_loader)
+            index, data, attr, _ = next(train_iter)
+
+        data = data.to(device)
+        attr = attr.to(device)
+
+        label = attr[:, target_attr_idx]
+
+        logit = model(data)
+        loss_per_sample = label_criterion(logit.squeeze(1), label)
+
+        loss = loss_per_sample.mean()
+
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        main_log_freq = 10
+        if step % main_log_freq == 0:
+            loss = loss.detach().cpu()
+            writer.add_scalar("loss/train", loss, step)
+
+            bias_attr = attr[:, bias_attr_idx]  # oracle
+            loss_per_sample = loss_per_sample.detach()
+            if (label == bias_attr).any().item():
+                aligned_loss = loss_per_sample[label == bias_attr].mean()
+                writer.add_scalar("loss/train_aligned", aligned_loss, step)
+
+            if (label != bias_attr).any().item():
+                skewed_loss = loss_per_sample[label != bias_attr].mean()
+                writer.add_scalar("loss/train_skewed", skewed_loss, step)
+
+        if step % main_valid_freq == 0:
+            valid_attrwise_accs = evaluate(model, valid_loader)
+            valid_attrwise_accs_list.append(valid_attrwise_accs)
+            valid_accs = torch.mean(valid_attrwise_accs)
+            writer.add_scalar("acc/valid", valid_accs, step)
+            # eye_tsr = torch.eye(num_classes)
+            # writer.add_scalar(
+            #     "acc/valid_aligned",
+            #     valid_attrwise_accs[eye_tsr > 0.0].mean(),
+            #     step
+            # )
+            # writer.add_scalar(
+            #     "acc/valid_skewed",
+            #     valid_attrwise_accs[eye_tsr == 0.0].mean(),
+            #     step
+            # )
+
+    os.makedirs(os.path.join(log_dir, "result", main_tag), exist_ok=True)
+    result_path = os.path.join(log_dir, "result", main_tag, "result.th")
+    valid_attrwise_accs_list = torch.stack(valid_attrwise_accs_list)
+    with open(result_path, "wb") as f:
+        torch.save({"valid/attrwise_accs": valid_attrwise_accs_list}, f)
+
+    model_path = os.path.join(log_dir, "result", main_tag, "model.th")
+    state_dict = {
+        'steps': step,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+    with open(model_path, "wb") as f:
+        torch.save(state_dict, f)
+
---
+++ llf/models/models.py
@@ -0,0 +1,267 @@
+'''
+This code is taken from https://github.com/kakaoenterprise/Learning-Debiased-Disentangled/blob/master/module/mlp.py
+
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+from torch.autograd import Variable
+
+import numpy as np
+
+from torchvision.models import resnet50
+
+__all__ = [
+    "ResNet",
+    "resnet20",
+    "resnet32",
+    "resnet44",
+    "resnet56",
+    "resnet110",
+    "resnet1202",
+]
+
+
+class MLP(nn.Module):
+    def __init__(self, num_classes = 10):
+        super(MLP, self).__init__()
+        self.feature = nn.Sequential(
+            nn.Linear(3*28*28, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 16),
+            nn.ReLU()
+        )
+        self.classifier = nn.Linear(16, num_classes)
+
+
+    def forward(self, x, mode=None, return_feat=False):
+        x = x.view(x.size(0), -1) / 255
+        feat = x = self.feature(x)
+        final_x = self.classifier(x)
+        if mode == 'tsne' or mode == 'mixup':
+            return x, final_x
+        else:
+            if return_feat:
+                return final_x, feat
+            else:
+                return final_x
+
+
+def _weights_init(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+        init.kaiming_normal_(m.weight)
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option="A"):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes,
+            planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == "A":
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(
+                    lambda x: F.pad(
+                        x[:, :, ::2, ::2],
+                        (0, 0, 0, 0, planes // 4, planes // 4),
+                        "constant",
+                        0,
+                    )
+                )
+            elif option == "B":
+                self.shortcut = nn.Sequential(
+                    nn.Conv2d(
+                        in_planes,
+                        self.expansion * planes,
+                        kernel_size=1,
+                        stride=stride,
+                        bias=False,
+                    ),
+                    nn.BatchNorm2d(self.expansion * planes),
+                )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(
+            3, 16, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.avgpool =  nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.fc = nn.Linear(64, num_classes)
+
+        self.apply(_weights_init)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def extract(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.avg_pool2d(out, out.size()[3])
+        feat = out.view(out.size(0), -1)
+
+        return feat
+
+    def predict(self, x):
+        prediction = self.fc(x)
+        return prediction
+
+    def forward(self, x, mode=None):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        # out = F.avg_pool2d(out, out.size()[3])
+        # out = out.view(out.size(0), -1)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        final_out = self.fc(out)
+        if mode == 'tsne' or mode == 'mixup':
+            return out, final_out
+        else:
+            return final_out
+
+
+def resnet20(num_classes):
+    return ResNet(BasicBlock, [3, 3, 3], num_classes)
+
+
+def resnet32():
+    return ResNet(BasicBlock, [5, 5, 5])
+
+
+def resnet44():
+    return ResNet(BasicBlock, [7, 7, 7])
+
+
+def resnet56():
+    return ResNet(BasicBlock, [9, 9, 9])
+
+
+def resnet110():
+    return ResNet(BasicBlock, [18, 18, 18])
+
+
+def resnet1202():
+    return ResNet(BasicBlock, [200, 200, 200])
+
+
+def test(net):
+    import numpy as np
+
+    total_params = 0
+
+    for x in filter(lambda p: p.requires_grad, net.parameters()):
+        total_params += np.prod(x.data.numpy().shape)
+    print("Total number of params", total_params)
+    print(
+        "Total layers",
+        len(
+            list(
+                filter(
+                    lambda p: p.requires_grad and len(p.data.size()) > 1,
+                    net.parameters(),
+                )
+            )
+        ),
+    )
+
+
+class GeneralizedCELoss(nn.Module):
+
+    def __init__(self, q=0.7):
+        super(GeneralizedCELoss, self).__init__()
+        self.q = q
+             
+    def forward(self, logits, targets):
+        p = F.softmax(logits, dim=1)
+        if np.isnan(p.mean().item()):
+            raise NameError('GCE_p')
+        Yg = torch.gather(p, 1, torch.unsqueeze(targets, 1))
+        # modify gradient of cross entropy
+        loss_weight = (Yg.squeeze().detach()**self.q)*self.q
+        if np.isnan(Yg.mean().item()):
+            raise NameError('GCE_Yg')
+
+        loss = F.cross_entropy(logits, targets, reduction='none') * loss_weight
+        return loss
+
+
+def get_model(model_tag, num_classes):
+    if model_tag == "ResNet20":
+        return resnet20(num_classes)
+
+    elif model_tag == "ResNet18":
+        print('bringing no pretrained resnet18 ...')
+        model = resnet18(pretrained=False)
+        model.fc = nn.Linear(512, num_classes)
+        return model
+    
+    elif model_tag == "ResNet50":
+        model = resnet50(pretrained=True)
+        for param in model.parameters():
+            param.requires_grad = False
+        model.fc = nn.Linear(2048, num_classes)
+        model.fc.weight.requires_grad = True
+        model.fc.bias.requires_grad = True
+        
+        return model
+
+    elif model_tag == "MLP":
+        return MLP(num_classes=num_classes)
+
+    else:
+        raise NotImplementedError
